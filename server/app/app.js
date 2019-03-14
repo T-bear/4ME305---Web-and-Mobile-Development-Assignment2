94a357b6c37d10452678814c2d526af7b88b6c06
T-bear <torbjorn.holgersson87@gmail.com>
2019-03-14 13:29:16 +0100

    added passport google auth from server.js

--- server/app/app.js
+++ server/app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const fileUpload = require('express-fileupload');
+const passport = require('passport');
 
 const app = express();
 
@@ -9,6 +10,9 @@ app.use(express.static('public'));
 // use 'extended' to avoid deprecation warning
 app.use(express.urlencoded({extended: true}));
 app.use(fileUpload({useTempFiles: true}));
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static DB settings
 const url = "mongodb://localhost:27017";
@@ -19,6 +23,8 @@ const collection_nameUsr = "users"
 // avoid deprecation warning
 const opt = {useNewUrlParser: true};
 
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 // This will hold the collection as a global, for use in the whole app
 var collection;
 
@@ -26,15 +32,28 @@ var collection;
 mongodb.MongoClient.connect(url, opt, function(err, client) {
   if(err) throw err;
 
-  const db = client.db(db_name);
+  const db = client.db(db_nameUsr);
   
   // Save the collection in the global var
-  collection = db.collection(collection_name);
+  collection = db.collection(collection_nameUsr);
 
   // Start the application after the database connection is ready
   app.listen(3000);
   console.log("Listening on port 3000");
 });
+
+//Google strategy
+passport.use(new GoogleStrategy({
+    clientID: '604880648774-4qbuienc8391qcjktj78qkpnrqjt16pr.apps.googleusercontent.com',
+    clientSecret: 'KvcJyJ4FVI4k3virSrtU5pMD',
+    callbackURL: 'http://localhost:8100/auth/google/callback'
+},
+    function (accessToken, refreshToken, profile, done) {
+        queryDatabase("INSERT IGNORE INTO user (email, Role_IDrole, Organization) VALUES ('" + profile.emails[0].value + "', 2, 1)");
+        return done(null, profile);
+       //});
+    }
+));
 
 // The root is "read" for CRUD
 app.get('/', function(req, res) {
@@ -54,3 +73,13 @@ app.post('/create', function(req, res) {
 app.get('/delete/:id', function(req, res) {
 
 });
+
+//Authenticating Google login
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/plus.login',
+            'email'] 
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google',  { successRedirect: '/tab2',
+                                                                    failureRedirect: '/tab1'}));
