e7cec5f2d33d6757454b299693294cadac6ae377
Finlirare <35921053+Finlirare@users.noreply.github.com>
2019-03-15 11:21:40 +0100

    Add files via upload

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+
+export class Tab3Page {
+	image: any;
+	savedImgs: any = [];
+	
+	constructor() {}
+//Using the camera to take a photo
+	async takePhoto(){
+			const result = await Camera.getPhoto({
+			quality: 90,
+			resultType: CameraResultType.Base64,
+			source: CameraSource.Camera
+	});
+	this.image = result.base64Data;
+	}
+
+
+}
